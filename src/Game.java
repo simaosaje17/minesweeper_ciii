be6c542fba49b415ca3137252887fedfee558fe6
James-NOVAIMS <20241409@novaims.unl.pt>
2025-11-27 14:21:26 +0000

    Initialization of the game

---
+++ src/Game.java
@@ -0,0 +1,61 @@
+import java.util.Random;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.ArrayList;   // a resizable array.  elements can be added and removed from an ArrayList whenever you want.
+import java.util.logging.Level;
+
+public class Game{
+    int tilesize ;
+    int numRow ;
+    int numCol = numRow;
+    int boardwidth = numCol * this.tilesize;
+    int boardheight = numRow * this.tilesize;
+
+
+    // making a window for the application
+    JFrame frame = new JFrame("Mineswepper");
+    JLabel textLable = new JLabel();
+    JPanel textPanel = new JPanel();
+
+
+
+    public Game(String level){
+
+        // different size for different level
+
+        if (level.equals("e")){
+            this.tilesize = 70;
+            this.numRow = 8;
+        }
+
+        //to avoid multiple checks using else if
+        else if (level.equals("m")){
+            this.tilesize = 35;
+            this.numRow = 16;
+        }
+        else if (level.equals("d")){
+            this.tilesize = 17;
+            this.numRow = 20;
+        }
+        else System.out.println("unknown");
+
+
+        // initialization
+        //initialized numCol, boardwidth, and boardheight after difficulty is chosen because
+        // if i had not done that then column, boardwidth adn board height would always be 0.
+        this.numCol = this.numRow; // square board
+        this.boardwidth = this.numCol * this.tilesize;
+        this.boardheight = this.numRow * this.tilesize;
+
+        frame.setSize(boardwidth,boardheight);
+        //frame.setLocation();
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);
+    }
+
+
+}
+
---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+
+    static void main(String[] args) {
+        Game mineswepper = new Game("m");
+
+    }
+
+}
